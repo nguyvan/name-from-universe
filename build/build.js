442e6745e8ac8bba202eb862d3a50af40fc40327
nguyvan <vantriet.nguyen93@gmail.com>
2022-02-26 10:43:11 +0100

    name from multiverse

---
+++ build/build.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    // "use strict";
+    // GetTableFromExcel("./data/liste_prénoms_arabo-musulmans.xlsx")
+    let valueDisplays = document.querySelectorAll(".number-window-item");
+    let interval = 0.01;
+    let i = 0;
+
+    valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(function(){
+            startValue += 1;
+            valueDisplay.textContent = startValue;
+            if (startValue == endValue) {
+                clearInterval(counter);
+            }
+        }, duration);
+    });
+
+    document.querySelector("#first-name").addEventListener('keypress', function(event) {
+        if (event.key == "Enter") {
+            fetch('http://192.168.0.27:5000/find', {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify({name: event.target.value})
+            }).then(response => response.json()).then(data => {
+                if (data.success) {
+                    document.getElementById("bottom-result-1").innerHTML = "Your first name is already arabized !"
+                    document.getElementById("bottom-result-2").innerHTML = ""
+                    document.getElementById("bottom-result-3").innerHTML = ""
+                }
+                else {
+                    document.getElementById("bottom-result-1").innerHTML = "This first name is not arabized !"
+                    document.getElementById("bottom-result-2").innerHTML = "Your new first name will be : "
+                    document.getElementById("bottom-result-3").innerHTML = data.name
+                }
+            }).catch(error => console.log(error))
+        }
+    }) 
+})
+
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const xlsx = require("xlsx");
+const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+const stringSimilarity = require("string-similarity");
+const jsonParser = bodyParser.json();
+
+const app = express();
+
+const workbook = xlsx.readFile('./build/data/liste_prénoms_arabo-musulmans.xlsx');
+const sheet_name_list = workbook.SheetNames;
+const listname = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]], {header: 1}).map((items) => {
+    return items[0].trim()
+});
+
+dotenv.config()
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/find', jsonParser, function(req, res){
+    let req_body = req.body;
+    let name = req_body.name;
+    if (listname.map((value)=>value.toLowerCase()).includes(name.toLowerCase())) {
+        return res.status(200).json({
+            success: true,
+            name: name
+        });
+    }
+    else {
+        let argstr = listname[0];
+        let max = stringSimilarity.compareTwoStrings(name.toLowerCase(), listname[0].toLowerCase());
+        listname.forEach((n) => {
+            if (stringSimilarity.compareTwoStrings(name.toLowerCase(), n.toLowerCase()) > max) {
+                max = stringSimilarity.compareTwoStrings(name.toLowerCase(), n.toLowerCase())
+                argstr = n
+            }
+        })
+        
+        return res.status(200).json({
+            success: false,
+            name: argstr
+        });
+    }
+})
+
+
+//add the router
+app.use(express.static(__dirname));
+
+app.listen(process.env.port, process.env.host, function(){
+    console.log(`Api up and running at: http://${process.env.host}:${process.env.port}`);
+});
+
