af1497d8160f038aa604ff03adba4739940287d3
nguyvan <vantriet.nguyen93@gmail.com>
2022-03-10 06:03:33 +0100

    fix

--- build/build.js
+++ build/build.js
@@ -18,32 +18,32 @@ $(document).ready(function() {
         }, duration);
     });
 
-    document.querySelector("#first-name").addEventListener('keypress', function(event) {
-        if (event.key == "Enter") {
-            fetch('https://name-from-multiverse.herokuapp.com/find', {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer',
-                body: JSON.stringify({name: event.target.value})
-            }).then(response => response.json()).then(data => {
-                if (data.success) {
-                    document.getElementById("bottom-result-1").innerHTML = "Your first name is already arabized !"
-                    document.getElementById("bottom-result-2").innerHTML = ""
-                    document.getElementById("bottom-result-3").innerHTML = ""
-                }
-                else {
-                    document.getElementById("bottom-result-1").innerHTML = "This first name is not arabized !"
-                    document.getElementById("bottom-result-2").innerHTML = "Your alternative first name will be : "
-                    document.getElementById("bottom-result-3").innerHTML = data.name
-                }
-            }).catch(error => console.log(error))
-        }
+    document.querySelector("#button-search").addEventListener('click', function() {
+        let first_name = document.getElementById("#first-name").value;
+        fetch('https://name-from-multiverse.herokuapp.com/find', {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify({name: event.target.value})
+        }).then(response => response.json()).then(data => {
+            if (data.success) {
+                document.getElementById("bottom-result-1").innerHTML = "Your first name is already arabized !"
+                document.getElementById("bottom-result-2").innerHTML = ""
+                document.getElementById("bottom-result-3").innerHTML = ""
+            }
+            else {
+                document.getElementById("bottom-result-1").innerHTML = "This first name is not arabized !"
+                document.getElementById("bottom-result-2").innerHTML = "Your alternative first name will be : "
+                document.getElementById("bottom-result-3").innerHTML = data.name
+            }
+        }).catch(error => console.log(error))
+        
     }) 
 })
